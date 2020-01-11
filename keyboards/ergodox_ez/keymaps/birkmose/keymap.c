7cc676ed66cc6ca09061db153918eba8f7bf2678
Michael Birkmose <micael.birkmose@gmail.com>
2020-01-11 12:55:23 +0100

    Added windows key for easy access

--- keyboards/ergodox_ez/keymaps/birkmose/keymap.c
+++ keyboards/ergodox_ez/keymaps/birkmose/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_RIGHT,         KC_6,               KC_7,               KC_8,       KC_9,           KC_0,                   KC_MINUS,
       KC_KP_SLASH,      KC_Y,               KC_U,               KC_I,       KC_O,           KC_P,                   NO_AM,
                         KC_H,               LT(2,KC_J),               KC_K,       KC_L,           LT(2,KC_SCOLON),        LGUI_T(KC_QUOTE),
-      KC_KP_SLASH,      KC_N,               KC_M,               KC_COMMA,   KC_DOT,         KC_SLASH,               KC_LALT,
+      KC_LGUI,      KC_N,               KC_M,               KC_COMMA,   KC_DOT,         KC_SLASH,               KC_LALT,
                                             LGUI_T(KC_ESCAPE),  KC_DOWN,    KC_LBRACKET,    KC_RBRACKET,            KC_A,
       KC_LALT,          KC_RALT,
       TD(TD_PG_UP_HOME),
